bfabff65313fa8925b888842b150eab7a349a96f
SatybaldievT <106535363+SatybaldievT@users.noreply.github.com>
2022-06-06 02:34:48 +0300

    06.06.2022

--- GO/gauss.go
+++ GO/gauss.go
@@ -5,32 +5,82 @@ import (
 	"os"
 )
 
-func gauss(smt [][]int) {
+type pair struct {
+	a, b int
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+func gcd(a, b int) int {
+	a, b = abs(a), abs(b)
+
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+
+	return a
+}
+
+func lcm(a, b int) int {
+	a, b = abs(a), abs(b)
+
+	return (a * b) / gcd(a, b)
+}
+func sum(a, b pair) pair {
+	q := b
+	g := gcd(q.b, a.b)
+	q.a = a.a*(q.b/g) + q.a*(a.b/g)
+	q.b = q.b * a.b / g
+	return q.norm()
+}
+func (a pair) norm() pair {
+	g := gcd(a.a, a.b)
+	a.a /= g
+	a.b /= g
+	if a.a < 0 && a.b < 0 {
+		a.a = -a.a
+		a.b = -a.b
+	}
+	if a.a > 0 && a.b < 0 {
+		a.a = -a.a
+		a.b = -a.b
+	}
+	return a
+}
+func sum_pair(q []pair, b []int) pair {
+	a := q[0]
+	a.a *= b[0]
+	for i := 1; i < len(q); i++ {
+
+		a = sum(a, pair{q[i].a * b[i], q[i].b})
+	}
+	return a
+}
+func gauss(smt [][]int) []pair {
 	if len(smt) == 1 {
 		//выход из рекурсии
-		/*l:=len(smt[0])
-		for i:=0;i<l-1;i++{
-			if smt[0][i] != 0 {return make([]pair,0),false}
-		}
-		if smt[0][l-1]==0 && smt[0][l]!=0 {return make([]pair,0),false}
-		if smt[0][l-1]==0 && smt[0][l]==0 {return append(make([]pair, 0), pair{0,1}),true}
-		return append(make([]pair, 0), pair{smt[0][l-1],smt[0][l]}),true*/
 		l := len(smt[0]) - 1
 		if l > 2 {
-			fmt.Println("ERROR")
-			return
+
+			return nil
 		}
 
 		if smt[0][l-1] == 0 {
-			fmt.Println("ERROR")
-			return
+
+			return nil
 		}
 		if smt[0][l] == 0 {
-			fmt.Println("ERROR")
-			return
+			return nil
 		}
 
-		fmt.Print(smt[0])
+		return append(make([]pair, 0), pair{smt[0][1], smt[0][0]}.norm())
 
 	} else {
 		a := smt[0]
@@ -42,8 +92,7 @@ func gauss(smt [][]int) {
 			}
 		}
 		if i == len(smt)-1 {
-			fmt.Println("ERROR")
-			return
+			return nil
 		}
 		smt[0] = smt[i]
 		smt[i] = a
@@ -53,12 +102,25 @@ func gauss(smt [][]int) {
 			}
 			smt[i][0] = 0
 		}
-		fmt.Println(smt[0])
+
+		a = make([]int, len(smt[0]))
+		copy(a, smt[0])
+
 		smt = smt[1:]
 		for i = 0; i < len(smt); i++ {
 			smt[i] = smt[i][1:]
 		}
-		gauss(smt)
+		q := gauss(smt)
+		if q == nil {
+			return nil
+		}
+
+		su := sum_pair(q, a[1:len(a)-1])
+
+		su.a *= -1
+		su = sum(su.norm(), pair{a[len(a)-1], 1})
+		su = pair{su.a, su.b * a[0]}
+		return append([]pair{su.norm()}, q...)
 	}
 
 }
@@ -74,6 +136,6 @@ func main() {
 
 		}
 	}
-	gauss(smt)
+	fmt.Println(gauss(smt))
 
 }
--- GO/test.go
+++ GO/test.go
@@ -1,19 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+type pair struct {
+	a, b int
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
 func gcd(a, b int) int {
-	for a != b {
-		if a > b {
-			a -= b
-		} else {
-			b -= a
-		}
+	a, b = abs(a), abs(b)
+
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
 	}
 
 	return a
 }
 
+func lcm(a, b int) int {
+	a, b = abs(a), abs(b)
+
+	return (a * b) / gcd(a, b)
+}
+func sum(a, b pair) pair {
+	q := b
+	g := gcd(q.b, a.b)
+	q.a = a.a*(q.b/g) + q.a*(a.b/g)
+	q.b = q.b * a.b / g
+	return q.norm()
+}
+func (a pair) norm() pair {
+	g := gcd(a.a, a.b)
+	a.a /= g
+	a.b /= g
+	return a
+}
+
+/*func sum_pair(a []pair, b []int) pair {
+
+	a[0].a *= b[0]
+	for i := 1; i < len(a); i++ {
+		a[i].a *= b[i]
+		a[0] = sum(a[0], a[i])
+	}
+	return a[0]
+}*/
+func sum_pair(q []pair, b []int) pair {
+	a := q[0]
+	a.a *= b[0]
+	for i := 1; i < len(q); i++ {
+
+		a = sum(a, pair{q[i].a * b[i], q[i].b})
+	}
+	return a
+}
 func main() {
-	fmt.Println(gcd(5, 15))
+	q := []pair{pair{214, 7}, pair{274, 21}}
+	a := []int{-4, -1, 8, 2}
+	fmt.Println(q, a, sum_pair(q, a[1:len(a)-1]))
 }
