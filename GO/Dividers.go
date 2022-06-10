c091f51b8339afd2589648031ccbbea44f10430e
SatybaldievT <106535363+SatybaldievT@users.noreply.github.com>
2022-06-10 09:41:20 +0300

    Update Dividers.go

--- GO/Dividers.go
+++ GO/Dividers.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
+	"sort"
 )
 
 func dev(a int, b []int) {
@@ -15,26 +15,63 @@ func dev(a int, b []int) {
 	}
 }
 func main() {
-	var a int
-	fmt.Fscanln(os.Stdin, &a)
-	seive := make([]bool, int(math.Sqrt(float64(a)))+1)
+	var n int
+	fmt.Fscanln(os.Stdin, &n)
+	q := n
+	a := []int{}
+	p := []int{}
+	d := 2
+	for d*d <= n {
+		if n%d == 0 {
+			if len(p) != 0 && p[len(p)-1] == d {
+				n = n / d
+				continue
+			}
+			p = append(p, d)
+			n = n / d
+		} else {
+			d += 1
+		}
+	}
 
-	limit := int(math.Sqrt(math.Sqrt(float64(a)))) + 1
+	if n > 1 {
+		if len(p) != 0 && p[len(p)-1] != n {
+			p = append(p, n)
+		}
+		if len(p) == 0 {
+			p = append(p, n)
+		}
+	}
+	for d = 1; d*d < q; d++ {
+		if q%d == 0 {
+			a = append(a, d)
+			a = append(a, q/d)
+		}
+	}
+	if d*d == q {
+		a = append(a, d)
+	}
+	sort.SliceStable(a, func(i, j int) bool {
+		return a[i] > a[j]
+	})
 
-	for i := 2; i < limit; i++ {
-		if !seive[i] {
-			for j := i * i; j < int(math.Sqrt(float64(a))); j += i {
-				seive[j] = true
+	fmt.Println("graph {")
+	for c, i := range a {
+		if c != 0 && a[c-1] == i {
+			continue
+		}
+		fmt.Printf("\t%d\n", i)
+	}
+	for c, i := range a {
+		if c != 0 && a[c-1] == i {
+			continue
+		}
+		for _, l := range p {
+			if i%l == 0 {
+				fmt.Printf("\t%d--%d\n", i, i/l)
 			}
 		}
 	}
-	p := make([]int, 0)
-	fmt.Println("graph {")
-	for i := 2; i < int(math.Sqrt(float64(a))); i++ {
-		if !seive[i] {
-			p = append(p, i)
-		}
-	}
-	dev(a, p)
+
 	fmt.Println("}")
 }
